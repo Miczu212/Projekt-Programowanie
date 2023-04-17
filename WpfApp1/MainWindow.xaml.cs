2c12216e6b279a7d68857ec32ed496f65a5b38a7
Miczu212 <gabs123489@gmail.com>
2023-04-17 20:02:25 +0200

    Dodano funkcje pozwalającą na prace nad 2 plikami textowymi

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -21,9 +21,10 @@ namespace WpfApp1
 
     public partial class MainWindow : Window
     {
-        string sciezka;
-        bool pierwszyraz = false;
-        string czyzmiana; // ta zmienna jest używana by sprawdzic czy zawartosc textboxa sie zmienila od czasu wczytania textboxa
+        string sciezka,sciezka2;
+
+        bool pierwszyraz = false,podzial=false;
+        string czyzmiana,czyzmiana2; // ta zmienna jest używana by sprawdzic czy zawartosc textboxa sie zmienila od czasu wczytania textboxa
         
         private void sprawdzenie(object s, System.ComponentModel.CancelEventArgs e)//trzeba zrobić sprawdzenie czy plik został zapisany a jak nie to wyswietlic messageboxa
         {
@@ -36,6 +37,11 @@ namespace WpfApp1
                     {
                         czyzmiana = okno.Text;
                         File.WriteAllText(sciezka, okno.Text);
+                        if(podzial==true)
+                        {
+                            czyzmiana2 = temp2.Text;
+                            File.WriteAllText(sciezka2, temp2.Text);
+                        }
                     }
                     catch (Exception ex)
                     {
@@ -80,6 +86,7 @@ namespace WpfApp1
         {
              if(pierwszyraz==false)
             {
+                podziel.Visibility = Visibility.Visible;
                 pierwszyraz = true;
                 okno.Visibility = Visibility.Visible;
                 Zapisz.Visibility = Visibility.Visible;
@@ -105,12 +112,41 @@ namespace WpfApp1
                 }
             }
         }
+        private void wybierzplik2 (object s, RoutedEventArgs e) //funkcja która pozwoli wczytać plik z górnego menu do textboxa
+        {
+            Zapisz_Jako_2.Visibility = Visibility.Visible;
+            temp2.Visibility = Visibility.Visible;
+            tempo2.Visibility = Visibility.Hidden;
+            var plikotwarcie = new Microsoft.Win32.OpenFileDialog();
+            plikotwarcie.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+
+            if (plikotwarcie.ShowDialog() == true)
+            {
+                sciezka2 = plikotwarcie.FileName;
+
+                try
+                {
+                    var tekst = File.ReadAllText(sciezka2);
+                    temp2.Text = tekst;
+                    czyzmiana2 = tekst;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Blad podczas otwierania pliku:");
+                }
+            }
+        }
         private void Zapis(object s, RoutedEventArgs e) //funkcja która pozwoli zapisać plik nad którym obecnie pracujemy
         {
             try
-            {
+            {  
                 czyzmiana = okno.Text;
                 File.WriteAllText(sciezka, okno.Text);
+                if(podzial==true)
+                {
+                    czyzmiana2 = temp2.Text;
+                    File.WriteAllText(sciezka2, temp2.Text);
+                }
                 MessageBox.Show("pomyslnie zapisano");
             }
             catch (Exception ex)
@@ -142,10 +178,41 @@ namespace WpfApp1
                 }
             }
         }
-        private void Nowy(object s, RoutedEventArgs e) // Funkcja która pozwoli stworzyc nowy plik, trzeba jednak wpierw wybrac folder w którym się zapisze i nazwe
+        private void Zapisjako2(object s, RoutedEventArgs e) //funkcja która pozwoli zapisać plik nad którym obecnie pracujemy w innym folderze
+        {   
+            var sciezkatemp = sciezka2;
+            var zapissciezka = new Microsoft.Win32.SaveFileDialog();
+            zapissciezka.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+
+            if (zapissciezka.ShowDialog() == true)
+            {
+                sciezka2 = zapissciezka.FileName;
+
+                try
+                {
+                    czyzmiana2 = temp2.Text;
+                    File.WriteAllText(sciezka2, temp2.Text);
+                    File.Delete(sciezkatemp);
+                    MessageBox.Show("pomyslnie zapisano");
+                }
+
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Blad podczas zapisywania pliku:");
+                }
+            }
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
         {
+            //Musze dodac okno które wyskakuje w którym mozna zmienic styl aplikacji
+        }
+
+        private void Nowy(object s, RoutedEventArgs e) // Funkcja która pozwoli stworzyc nowy plik, trzeba jednak wpierw wybrac folder w którym się zapisze i nazwe
+        {   
             if (pierwszyraz == false)
             {
+                podziel.Visibility = Visibility.Visible;
                 pierwszyraz = true;
                 okno.Visibility = Visibility.Visible;
                 Zapisz.Visibility = Visibility.Visible;
@@ -164,7 +231,33 @@ namespace WpfApp1
                     File.WriteAllText(sciezka, string.Empty);
                     okno.Text = string.Empty;
                     czyzmiana = okno.Text;
-                    MessageBox.Show("pomyslnie zapisano");
+                   
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Blad podczas zapisywania pliku:");
+                }
+            }
+        }
+        private void Nowy2(object s, RoutedEventArgs e) // Funkcja która pozwoli stworzyc nowy plik, trzeba jednak wpierw wybrac folder w którym się zapisze i nazwe
+        {
+            Zapisz_Jako_2.Visibility = Visibility.Visible;
+            tempo2.Visibility = Visibility.Hidden;
+            temp2.Visibility = Visibility.Visible;
+            var zapissciezka = new Microsoft.Win32.SaveFileDialog();
+            zapissciezka.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+
+            if (zapissciezka.ShowDialog() == true)
+            {
+                sciezka2 = zapissciezka.FileName;
+
+                try
+                {
+                    File.WriteAllText(sciezka2, string.Empty);
+                    temp2.Text = string.Empty;
+                    czyzmiana2 = temp2.Text;
+                  
 
                 }
                 catch (Exception ex)
@@ -174,16 +267,19 @@ namespace WpfApp1
             }
         }
 
-        private void Button_Click(object s, RoutedEventArgs e) //funkcja do zaimplementowania w bliskiej przyszłości
-        {   
+        private void Podziel(object s, RoutedEventArgs e) //funkcja do zaimplementowania w bliskiej przyszłości
+        {
             //podzial na 2 osobne okna nad którymi mozna pracowac
+            nowy2.Visibility = Visibility.Visible;
+            podzial = true;
+            podziel.Visibility = Visibility.Hidden;
             okno.Visibility=Visibility.Hidden;
+            temp1.Text = okno.Text;
             okno.Name = "oknowyl";
+            otworz2.Visibility = Visibility.Visible;
+            tempo2.Visibility = Visibility.Visible;
             temp1.Visibility = Visibility.Visible;
-            temp2.Visibility = Visibility.Visible;
             temp1.Name = "okno";
-            
-           
         }
     }
 }
